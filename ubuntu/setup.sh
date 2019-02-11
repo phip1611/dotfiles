81cdd140be1ceb29d57d4c0d20f6e030c1cee1d6
Philipp Schuster <phip1611@gmail.com>
2019-02-11 11:22:41 +0100

    setup sh git config

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -6,3 +6,10 @@ apt install -y zsh tree curl git vim build-essential valgrind
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+curl https://getmic.ro | bash
+mv micro /usr/local/bin
+
+git config --global user.email "phip1611@gmail.com"
+git config --global user.name "Philipp Schuster"
+git config --global core.editor micro
+git config --global pull.rebase true

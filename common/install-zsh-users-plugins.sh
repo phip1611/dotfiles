8f40b3e93d08ca355864cc95387a0e59f7d975d9
Philipp Schuster <phip1611@gmail.com>
2019-05-04 18:05:33 +0200

    oh-my-zsh plugins autosuggestions und syntaxhighlightning

---
+++ common/install-zsh-users-plugins.sh
@@ -0,0 +1,7 @@
+# Nach dem man "oh my zsh" normal installiert hat:
+
+# "zsh-syntax-highlightning" oh my zsh plugin
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# "zsh-syntax-highlightning" oh my zsh plugin
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions

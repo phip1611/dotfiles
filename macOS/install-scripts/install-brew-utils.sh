a9e7b62b6193b975a57053e93929aeec07e5dfa5
Philipp Schuster <phip1611@gmail.com>
2020-09-17 17:48:42 +0200

    macOS: more cool utils

--- macOS/install-scripts/install-brew-utils.sh
+++ macOS/install-scripts/install-brew-utils.sh
@@ -8,3 +8,9 @@ brew install coreutils findutils gnu-tar gnu-sed gawk gnutls gnu-indent gnu-geto
 brew install bat calc cmake cmatrix curl exa ffmpeg gcc git git-delta gpg htop maven micro node openjdk ripgrep tldr tree wget yarn zsh
 
 brew install fortune cowsay lolcat
+
+# cool statistics about code: https://github.com/xampprocky/tokei
+brew install tokei
+
+# modern replacement for "ps"
+brew install procs

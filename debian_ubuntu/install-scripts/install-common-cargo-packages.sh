5b41aab2fced5d26b72228f23907407d4530e34e
Philipp Schuster <phip1611@gmail.com>
2021-01-21 22:16:40 +0100

    added nflz cargo util to install scripts

--- debian_ubuntu/install-scripts/install-common-cargo-packages.sh
+++ debian_ubuntu/install-scripts/install-common-cargo-packages.sh
@@ -1,4 +1,4 @@
 echo "make sure rustup/rust/cargo is installed"
 echo "cargo acts also as package manager (for the local user only) :)"
 
-cargo install cargo-expand cargo-outdated cargo-update bat git-delta exa wambo 
+cargo install cargo-expand cargo-outdated cargo-update bat git-delta exa nflz wambo 
--- macOS/install-scripts/install-cargo-utils.sh
+++ macOS/install-scripts/install-cargo-utils.sh
@@ -4,7 +4,7 @@
 echo "Installs common cargo binaries"
 echo "make sure rustup/cargo is installed!"
 
-cargo install mac-sys-info cargo-expand cargo-update cargo-outdated wambo
+cargo install mac-sys-info cargo-expand cargo-update cargo-outdated nflz wambo
 
 set -e 
 set -x

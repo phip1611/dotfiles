1c21a81e791c7cba5344959030a0c1b2ad9a61bd
Philipp Schuster <phip1611@gmail.com>
2021-02-17 10:54:20 +0100

    macOS: setup µC dev with homebrew

---
+++ macOS/install-scripts/install-embedded-ucontroller-dev.sh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+# ^ this is mac os default/system shell; not the one from brew
+
+echo "installs necessary tools for embedded development (openocd, arm eabi gcc)"
+echo "install homebrew first!"
+
+set -e 
+set -x
+
+# install brew tap ArmMbed/homebrew-formulae
+#brew tap ArmMbed/homebrew-formulae
+#brew install arm-none-eabi-gcc
+# afterwards "arm-none-eabi-gcc" is a standalone binary
+brew install --cask gcc-arm-embedded
+
+# builds open-ocd from source to have the latest boards available
+brew install --HEAD open-ocd
+
+echo "Enter in CLion the following path for OpenOCD (realpath $(which openocd)):"
+realpath $(which openocd)

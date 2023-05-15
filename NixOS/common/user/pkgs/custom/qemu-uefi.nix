d2f5ec099345e4b00508f27f131258c480a88140
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2023-05-15 12:52:45 +0200

    NixOS: qemu-uefi fixed

--- NixOS/common/user/pkgs/custom/qemu-uefi.nix
+++ NixOS/common/user/pkgs/custom/qemu-uefi.nix
@@ -3,6 +3,6 @@
 # Wrapper around "qemu-system-x86_64" that uses OVMF as UEFI firmware.
 writeShellScriptBin "qemu-uefi" ''
   exec ${qemu}/bin/qemu-system-x86_64 \
-        -bios ${OVMF.firmware} \
+        -bios ${OVMF.fd}/FV/OVMF.fd \
         "$@"
 ''

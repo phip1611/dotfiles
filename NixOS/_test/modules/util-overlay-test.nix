dbea910f68c55cb22d150a7f11456a2d8ebe1053
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2023-05-19 13:28:31 +0200

    NixOS: util-overlay: better test trace function

--- NixOS/_test/modules/util-overlay-test.nix
+++ NixOS/_test/modules/util-overlay-test.nix
@@ -4,13 +4,18 @@ let
   username = config.phip1611.username;
 
   # From overlay. Make sure that is used at least once.
-  trace = pkgs.phip1611-util.tracePrettyVal;
+  trace = pkgs.phip1611-util;
 in
 {
   phip1611.util-overlay.enable = true;
 
   # Combo test: "tracePrettyVal" + writeZxScriptBin
-  users.users."${username}".packages = trace [
-    (pkgs.writers.writeZxScriptBin "foo" ''console.log(1)'')
-  ];
+  users.users."${username}".packages =
+    trace.tracePretty
+      "Trace function works"
+      trace.tracePrettyVal
+      [
+        (pkgs.writers.writeZxScriptBin "foo" ''console.log(1)'')
+      ]
+  ;
 }

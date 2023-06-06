fc3565fd5005460bf68682d69b1c5298a8453b5e
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2023-06-06 15:28:11 +0200

    NixOS: add nix-ld

--- NixOS/common/system/default.nix
+++ NixOS/common/system/default.nix
@@ -24,5 +24,16 @@ in
     phip1611.common.system.nix-cfg.enable = true;
     phip1611.common.system.nix-path-from-flake.enable = true;
     phip1611.common.system.sudo.enable = true;
+
+    # When unpatched dynamically linked programs are executed, they fail with
+    # file not found. Usually, the file "/lib64/ld-linux-x86-x64.so.2" is not
+    # found. This NixOS package adds a compatibility layer for that case. Alone,
+    # out of the box, this configuration makes no such program work, but it
+    # assists trouble shooting by printing:
+    #   cannot execute ...: You are trying to run an unpatched binary
+    #   on nixos, but you have not configured NIX_LD ... See ... for more
+    #   details.
+    # https://github.com/Mic92/nix-ld
+    programs.nix-ld.enable = true;
   };
 }

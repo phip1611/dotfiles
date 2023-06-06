436fdfc602033220f4e57aff74ab3a24dc7a5499
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2023-06-06 15:07:26 +0200

    ci: NixOS: pure nix-chell

--- NixOS/_test/test-build.sh
+++ NixOS/_test/test-build.sh
@@ -4,7 +4,11 @@ DIR=$(dirname "$(realpath "$0")")
 cd "$DIR" || exit
 
 # Builds the "ci" attribute of the local flake.
-nix-shell -p nixos-rebuild --run "nixos-rebuild build --flake .#ci"
+PKGS="git nixos-rebuild cacert"
+nix-shell --pure \
+    -p git nixos-rebuild cacert \
+    --run "nixos-rebuild build --flake .#ci"
+
 # There is no "--no-out-link" or similar. Hence, we manually remove
-# the out link.
+# the ./result folder.
 rm ./result

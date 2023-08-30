e9c9189030199431891e0663405c671d8d570bcd
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2023-08-30 10:33:28 +0200

    NixOS: common: env: zsh: more history options

--- NixOS/common/user/env/zsh.nix
+++ NixOS/common/user/env/zsh.nix
@@ -25,6 +25,37 @@ in
       programs.zsh = {
         enable = true;
 
+        # Context:
+        # - https://www.soberkoder.com/better-zsh-history/
+        # - https://zsh.sourceforge.io/Doc/Release/Options.html
+        history =
+          let
+            # Default is 10000.
+            saveLines = 200000; # This results in rouglhy 1-6 MiB memory usage.
+          in
+          {
+            # Save timestamp into the history file.
+            extended = true;
+            # If a new command line being added to the history list duplicates
+            # an older one, the older command is removed from the list (even if
+            # it is not the previous event).
+            # TODO this is on home-manager master but not on release-23.05.
+            #  ADD ONCE it is stable.
+            # ignoreAllDups = true;
+            ignoreDups = true; # TODO Reevaluate once the option above is used.
+            # Do not enter command lines into the history list if the first
+            # character is a space.
+            ignoreSpace = true;
+            # Number of history lines to save.
+            save = saveLines;
+            # Number of history lines to load into memory.
+            size = saveLines;
+            # Share command history between zsh sessions. This also adds new
+            # commands to the history as they are typed. Hence, this is more
+            # powerful than INC_APPEND_HISTORY.
+            share = true;
+          };
+
         oh-my-zsh = {
           enable = true;
           theme = "agnoster";

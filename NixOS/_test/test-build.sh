7cd67b75fc11aa5677ac05db53defb158f308fed
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2023-09-21 10:22:19 +0200

    NixOS: test: don't fail on rm if nixos-rebuild failed
    The rm failed in CI, nixos-rebuild produced a "no space left on device"
    error.

--- NixOS/_test/test-build.sh
+++ NixOS/_test/test-build.sh
@@ -4,11 +4,10 @@ DIR=$(dirname "$(realpath "$0")")
 cd "$DIR" || exit
 
 # Builds the "ci" attribute of the local flake.
-PKGS="git nixos-rebuild cacert"
 nix-shell --pure \
     -p git nixos-rebuild cacert \
     --run "nixos-rebuild build --flake .#ci"
 
 # There is no "--no-out-link" or similar. Hence, we manually remove
 # the ./result folder.
-rm ./result
+rm -f ./result

33a87fbd1df502e4125ca25718ac5c48f1b65961
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2023-10-11 18:53:46 +0200

    ci: NixOS: fix test-build.sh script

--- NixOS/_test/test-build.sh
+++ NixOS/_test/test-build.sh
@@ -3,10 +3,13 @@
 DIR=$(dirname "$(realpath "$0")")
 cd "$DIR" || exit
 
+set -euo pipefail
+IFS=$'\n\t'
+
 # Builds the "ci" attribute of the local flake.
 nix-shell --pure \
     -p git nixos-rebuild cacert \
-    --run "nixos-rebuild build --flake .#ci"
+    --run "nixos-rebuild build --flake .#ci" || (echo "❌ FAILURE" && exit 1)
 
 # There is no "--no-out-link" or similar. Hence, we manually remove
 # the ./result folder.
